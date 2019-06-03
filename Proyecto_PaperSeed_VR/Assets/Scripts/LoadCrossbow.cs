8b4fd923187c72f79e43c39bddfee9e2da93f9aa
Jaime <jpintomontalva@gmail.com>
2019-06-03 19:21:35 -0400

    testeo de colliders en el master

---
+++ Proyecto_PaperSeed_VR/Assets/Scripts/LoadCrossbow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadCrossbow : MonoBehaviour
+{
+
+	public MonoBehaviour VC;
+	public bool handInXbow = false;
+	public bool arrowInXbow = false;
+	public Transform arrowPosition;
+	public bool arrowInHand;
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+
+		if (other.tag == "ArrowPicker" && !handInXbow && VC.GetComponent<VirtualCarcajScript>().ArrowInHand)
+		{
+			handInXbow = true;
+
+			if (!arrowInXbow)
+			{
+				GameObject arrow = GameObject.Find("ARROW");
+				arrow.transform.SetParent(arrowPosition);
+				arrow.transform.position = arrowPosition.position;
+				arrow.transform.rotation = arrowPosition.rotation;
+				arrowInXbow = true;
+
+				VC.GetComponent<VirtualCarcajScript>().ArrowInHand = false;
+			}
+
+		}
+
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		handInXbow = false;
+	}
+}
